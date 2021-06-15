456f82f2ea98fff537834d35f34ac31d368f5069
Björn Busse <bj.rn@baerlin.eu>
2021-06-15 11:44:28 +0200

    ipv6: Add reverse lookup for AAAA records

--- handler.go
+++ handler.go
@@ -15,6 +15,8 @@ func (l *Ldap) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 	// opt := plugin.Options{}
 	state := request.Request{W: w, Req: r}
 
+	var result file.Result
+
 	zone := plugin.Zones(l.Zones.Names).Matches(state.Name())
 	if zone == "" {
 		return plugin.NextOrFailure(l.Name(), l.Next, ctx, w, r)
@@ -25,15 +27,30 @@ func (l *Ldap) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 		return dns.RcodeServerFailure, nil
 	}
 
-	var result file.Result
-
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Authoritative = true
 
-	l.zMu.RLock()
-	m.Answer, m.Ns, m.Extra, result = Zone.Lookup(ctx, state, state.Name())
-	l.zMu.RUnlock()
+	if r.Question[0].Qtype == dns.TypePTR {
+		hdr := dns.RR_Header{Name: r.Question[0].Name, Ttl: 3600, Class: dns.ClassINET, Rrtype: dns.TypePTR}
+
+		if ptr, found := l.Ptr[r.Question[0].Name]; found {
+			l.zMu.RLock()
+			m.Answer = []dns.RR{&dns.PTR{Hdr: hdr, Ptr: ptr}}
+			l.zMu.RUnlock()
+			result = file.Success
+		} else {
+			ptr := r.Question[0].Name
+			l.zMu.RLock()
+			m.Answer = []dns.RR{&dns.PTR{Hdr: hdr, Ptr: ptr}}
+			l.zMu.RUnlock()
+			result = file.Success
+		}
+	} else {
+		l.zMu.RLock()
+		m.Answer, m.Ns, m.Extra, result = Zone.Lookup(ctx, state, state.Name())
+		l.zMu.RUnlock()
+	}
 
 	if len(m.Answer) == 0 && result != file.NoData && l.Fall.Through(state.Name()) {
 		return plugin.NextOrFailure(l.Name(), l.Next, ctx, w, r)
--- sync.go
+++ sync.go
@@ -3,12 +3,13 @@ package ldap
 import (
 	"context"
 	"fmt"
-	"strings"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/file"
+	"github.com/miekg/dns"
 )
 
 // Run updates the zone from ldap.
@@ -93,6 +94,7 @@ func (l *Ldap) mapLdapRecordsToZone(ldapRecords []ldapRecord) (ldapRecordsPerZon
 }
 
 func (l *Ldap) fetchLdapRecords() (ldapRecords []ldapRecord, err error) {
+	l.Ptr = make(map[string]string)
 	searchResult, err := l.Client.SearchWithPaging(l.SearchRequest, l.pagingLimit)
 	if err != nil {
 		return nil, fmt.Errorf("fetching data from server: %w", err)
@@ -106,8 +108,12 @@ func (l *Ldap) fetchLdapRecords() (ldapRecords []ldapRecord, err error) {
 		}
 		ldapRecords[i] = ldapRecord{
 			fqdn: fqdn,
-			ip4:   net.ParseIP(searchResult.Entries[i].GetAttributeValue(l.Ip4Attr)),
-			ip6:   net.ParseIP(searchResult.Entries[i].GetAttributeValue(l.Ip6Attr)),
+			ip4:  net.ParseIP(searchResult.Entries[i].GetAttributeValue(l.Ip4Attr)),
+			ip6:  net.ParseIP(searchResult.Entries[i].GetAttributeValue(l.Ip6Attr)),
+		}
+		ipv6Arpa, err := dns.ReverseAddr(net.ParseIP(searchResult.Entries[i].GetAttributeValue(l.Ip6Attr)).String())
+		if err == nil {
+			l.Ptr[ipv6Arpa] = fqdn
 		}
 	}
 
